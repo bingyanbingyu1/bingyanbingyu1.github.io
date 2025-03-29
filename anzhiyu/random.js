951eda68439c9edef172a54bd88ceee85db0f0ed
bingyanbingyu1 <bingyanbingyu1@gmail.com>
2025-03-30 07:35:26 +0800

    Site updated: 2025-03-30 07:35:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/29/博客配置教学/","2025/03/29/Serv00-Hostuno-sb-yg一键脚本/","2025/03/29/HexoBlogNo1/","2025/03/29/hello-world/","2025/03/29/HexoBlogNo2/"];function toRandomPost(){
+var posts=["2025/03/30/雷电4模拟器一键宏教程/","2025/03/29/HexoBlogNo1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
