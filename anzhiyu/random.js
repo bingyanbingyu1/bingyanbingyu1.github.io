b2024e39069d46e04e6d99d9c73412e9f5d9c2c2
bingyanbingyu1 <bingyanbingyu1@gmail.com>
2025-04-01 18:37:12 +0800

    Site updated: 2025-04-01 18:37:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/30/雷电4模拟器一键宏教程/","2025/03/29/HexoBlogNo1/"];function toRandomPost(){
+var posts=["2025/03/30/雷电4模拟器一键宏教程/","2025/03/29/HexoBlogNo1/","2023/04/01/Cursor优化与MCP使用指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
