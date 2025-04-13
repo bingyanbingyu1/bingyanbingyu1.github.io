9da94e7f69e2b72c8decd1b027c4a41677cb34a5
bingyanbingyu1 <bingyanbingyu1@gmail.com>
2025-04-14 07:47:03 +0800

    Site updated: 2025-04-14 07:47:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/13/quake360抓取订阅/","2025/03/30/雷电4模拟器一键宏教程/","2025/03/29/HexoBlogNo1/","2023/04/01/Cursor优化与MCP使用指南/"];function toRandomPost(){
+var posts=["2025/04/14/【教程】MCP-保姆级教程/","2025/04/13/quake360抓取订阅/","2025/03/30/雷电4模拟器一键宏教程/","2025/03/29/HexoBlogNo1/","2023/04/01/Cursor优化与MCP使用指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
